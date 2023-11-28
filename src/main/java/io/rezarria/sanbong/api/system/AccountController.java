7eb30599cb1d836abaf28be4fbe00d2bb6309f77
rezarria <votanamcb@outlook.com>
2023-11-29 03:32:49 +0700

    thêm api lấy size của account

--- src/main/java/io/rezarria/sanbong/api/system/AccountController.java
+++ src/main/java/io/rezarria/sanbong/api/system/AccountController.java
@@ -1,23 +1,32 @@
 package io.rezarria.sanbong.api.system;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.fge.jsonpatch.JsonPatch;
-import io.rezarria.sanbong.model.Account;
-import io.rezarria.sanbong.repository.AccountRepository;
-import io.rezarria.sanbong.security.service.AccountService;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
-import java.util.Optional;
-import java.util.UUID;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+
+import io.rezarria.sanbong.model.Account;
+import io.rezarria.sanbong.repository.AccountRepository;
+import io.rezarria.sanbong.security.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,8 +41,14 @@ public class AccountController {
     @Lazy
     private final AccountRepository accountRepository;
 
+    @GetMapping("size")
+    public ResponseEntity<Long> getSize() {
+        return ResponseEntity.ok(accountService.getSize());
+    }
+
     @GetMapping(produces = "application/json")
-    public ResponseEntity<Collection<Account>> find(@RequestParam("current") Optional<Integer> current, @RequestParam("pageSize") Optional<Integer> pageSize) {
+    public ResponseEntity<Collection<Account>> find(@RequestParam("current") Optional<Integer> current,
+            @RequestParam("pageSize") Optional<Integer> pageSize) {
         if (current.isPresent() && pageSize.isPresent()) {
             var page = accountRepository.findAll(Pageable.ofSize(pageSize.get()).withPage(current.get()));
             return ResponseEntity.ok(page.stream().toList());
@@ -43,7 +58,8 @@ public class AccountController {
 
     @PostMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<Account> create(@RequestBody CreateDTO dto) {
-        return ResponseEntity.ok(accountService.add(Account.builder().username(dto.username).password(passwordEncoder.encode(dto.password)).build()));
+        return ResponseEntity.ok(accountService
+                .add(Account.builder().username(dto.username).password(passwordEncoder.encode(dto.password)).build()));
     }
 
     @SneakyThrows
