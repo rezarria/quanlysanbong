9bc2740abfb14eda6a7d887ac63527cef576f8e3
rezarria <votanamcb@outlook.com>
2023-12-31 12:27:28 +0700

    hoàn thành getPage

--- repository/src/main/java/io/rezarria/repository/CustomRepositoryImpl.java
+++ repository/src/main/java/io/rezarria/repository/CustomRepositoryImpl.java
@@ -3,10 +3,13 @@ package io.rezarria.repository;
 import io.rezarria.repository.interfaces.CustomRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Tuple;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Selection;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
@@ -21,11 +24,13 @@ public class CustomRepositoryImpl<O, K> extends SimpleJpaRepository<O, K> implem
     public CustomRepositoryImpl(JpaEntityInformation<O, ?> entityInformation, EntityManager entityManager) {
         super(entityInformation, entityManager);
         this.domainClass = entityInformation.getJavaType();
+        this.entityManager = entityManager;
     }
 
     public CustomRepositoryImpl(Class<O> domainClass, EntityManager entityManager) {
         super(domainClass, entityManager);
         this.domainClass = domainClass;
+        this.entityManager = entityManager;
     }
 
     @Override
@@ -43,11 +48,30 @@ public class CustomRepositoryImpl<O, K> extends SimpleJpaRepository<O, K> implem
 
     @Override
     public <T> Page<T> getPage(Pageable page, Class<T> type) {
-        var query = entityManager.getCriteriaBuilder().createQuery(domainClass);
-        var root = query.from(domainClass);
-        query.select(root);
-        entityManager.createQuery(query);
-        return null;
-    }
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tuple> criteriaQuery = criteriaBuilder.createTupleQuery();
 
+        Root<?> root = criteriaQuery.from(domainClass);
+
+        var select = criteriaQuery.multiselect(root.getModel().getDeclaredSingularAttributes().stream().map(attr -> root.get(attr.getName())).toArray(Selection[]::new));
+
+        var query = entityManager.createQuery(select);
+
+        query.setFirstResult((int) page.getOffset());
+        query.setMaxResults(page.getPageSize());
+
+        var resultList = query.getResultList();
+
+        var result = resultList.stream()
+                .map(tuple -> {
+                    return entityManager.getReference(type, tuple);
+                })
+                .toList();
+
+        CriteriaQuery<Long> countQuery = criteriaBuilder.createQuery(Long.class);
+        countQuery.select(criteriaBuilder.count(countQuery.from(domainClass)));
+        Long totalCount = entityManager.createQuery(countQuery).getSingleResult();
+
+        return new PageImpl<>(result, page, totalCount);
+    }
 }
