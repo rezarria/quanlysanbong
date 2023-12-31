3537c59c5fd071b01a462fe69d5c2fc667f62300
rezarria <votanamcb@outlook.com>
2023-12-31 12:26:01 +0700

    hoàn chỉnh getStreamByName

--- service/src/main/java/io/rezarria/service/ConsumerProductService.java
+++ service/src/main/java/io/rezarria/service/ConsumerProductService.java
@@ -2,23 +2,26 @@ package io.rezarria.service;
 
 import io.rezarria.model.ConsumerProduct;
 import io.rezarria.repository.ConsumerProductRepository;
+import io.rezarria.repository.OrganizationRepository;
 import io.rezarria.security.component.Auth;
 import io.rezarria.service.interfaces.IService;
 import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Stream;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class ConsumerProductService implements IService<ConsumerProductRepository, ConsumerProduct> {
 
     @Lazy
     private final ConsumerProductRepository repository;
+
+    private final OrganizationRepository organizationRepository;
+
     @Lazy
     private final EntityManager entityManager;
 
@@ -27,10 +30,24 @@ public class ConsumerProductService implements IService<ConsumerProductRepositor
         return repository;
     }
 
+    @Transactional(readOnly = true)
     public <T> Stream<T> getStream(Class<T> type) {
         Auth auth = new Auth();
         if (auth.hasRole("SUPER_ADMIN"))
             return repository.findAllStream(type);
+        return repository.findByOrganization_Accounts_Id(auth.getAccountId(), type);
+    }
+
+    @Transactional(readOnly = true)
+    public <T> Stream<T> getStreamByName(String name, Class<T> type) {
+        Auth auth = new Auth();
+        if(auth.hasRole("SUPER_ADMIN")) {
+            return repository.findAllByNameContaining(name, type);
+        }
+        var organizationId = organizationRepository.getIdByAccountId(auth.getAccountId());
+        if (organizationId.isPresent()) {
+            return repository.findByNameContainsAndCreatedBy_ActiveTrueAndOrganization_Id(name, auth.getAccountId(), type);
+        }
         return repository.findByOrganization_Accounts_Id(auth.getAccountId(), type);
     }
 
